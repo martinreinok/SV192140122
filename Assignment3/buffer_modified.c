49fc0de01d561de8addaa0174df8380b20b12479
Martin Reinok <78325056+martinreinok@users.noreply.github.com>
2022-11-03 19:23:21 +0100

    Update buffer_modified.c

--- Assignment3/buffer_modified.c
+++ Assignment3/buffer_modified.c
@@ -10,14 +10,33 @@ int head = 0;
 int tail = 0;
 int const maxlen = 4;
 
-/*@ ghost
-    int ghost_head = 0;
-	int ghost_tail = 0;
-*/
+/*@
+requires maxlen >= 0 && \valid(buffer+(0..maxlen-1));
+requires \valid(output_arr);
+requires 0 <= tail < maxlen;
+requires 0 <= head < maxlen;
+assigns tail, *output_arr;
 
-/*@ // Exercise I
-requires maxlen >= 0 && \valid(buffer+(0..maxlen));
-assigns tail, *output_arr, ghost_tail;
+behavior good:
+	assumes head != tail && tail + 1 < maxlen;
+	ensures tail == \old(tail) + 1;
+	ensures *output_arr == buffer[\old(tail)];
+	ensures \result == 0;
+	
+
+behavior bad:
+	assumes head == tail;
+	ensures \result == -1;
+
+behavior reset_buffer:
+	assumes tail != head && tail + 1 >= maxlen;
+	ensures tail + 1 == maxlen ==> tail == 0;
+	ensures *output_arr == buffer[\old(tail)];
+	ensures \result == 0;
+	
+
+complete behaviors;
+disjoint behaviors;
 */
 int pop(int *output_arr) {
 	int next;
@@ -28,27 +47,29 @@ int pop(int *output_arr) {
 		next = 0;
 	*output_arr = buffer[tail];
 	tail = next;
-	/*@ ghost
-        ghost_tail = tail;
-    */
+
    	// Exercise IV
-	//@assert \forall integer i; 2 <= i < head && maxlen > 2 ==> buffer[head] == buffer[head-1] + buffer[head-2];
+	// assert \forall integer i; 2 <= i < head && maxlen > 0 ==> buffer[head] == buffer[head-1] + buffer[head-2];
 
 	return 0;
 }
 
 /*@ // Exercise I
-requires maxlen >= 0 && \valid(buffer+(0..maxlen));
-requires head < maxlen;
-assigns head, buffer[head], ghost_head;
+requires maxlen >= 0 && \valid(buffer+(0..maxlen-1));
+requires 0 <= tail < maxlen;
+requires 0 <= head < maxlen;
+assigns head, buffer[0..head];
 
 behavior good:
-	assumes head + 1 < maxlen && head + 1 != tail || head + 1 >= maxlen && tail != 0;
+	assumes head + 1 < maxlen && head + 1 != tail;
+	ensures 0 <= head + 1 < maxlen ==> head == \old(head) + 1;
+	ensures buffer[\old(head)] == data;
 	ensures \result == 0;
 
 behavior reset_buffer:
 	assumes head + 1 >= maxlen && tail != 0;
-	ensures head == 0;
+	ensures head + 1 >= maxlen ==> head == 0;
+	ensures buffer[\old(head)] == data;
 	ensures \result == 0;
 
 behavior bad:
@@ -67,11 +88,8 @@ int push(int data) {
 		return -1; // buffer is full
 	buffer[head] = data;
 	head = next;
-	/*@ ghost
-        ghost_head = head;
-    */
 	// Exercise IV
-	//@assert \forall integer i; 2 <= i < head && maxlen > 2 ==> buffer[head] == buffer[head-1] + buffer[head-2];
+	// assert \forall integer i; 2 <= i <= head && maxlen > 2 ==> buffer[head] == buffer[head-1] + buffer[head-2];
 
 	return 0;
 }
@@ -81,19 +99,26 @@ WP will not attempt to prove the preconditions of the main function,
 since it cannot make any assumption about its calling context.
 https://stackoverflow.com/questions/66867596/frama-c-wp-and-preconditions
 */
+
+/*@
+assigns head, tail, buffer;
+*/
 int main() {
 	int data[4];
 	buffer = data;
 	int out[4];
 	int a = 1;
 	int b = 1;
+	//@ assert head == 0;
 	push(a);
+	//@ assert head == (maxlen > 2) ? 1 : 0;
 	push(b);
 
-	/*@ // Exercise II
+	/*@
 	loop invariant 2 <= i <= maxlen-1;	// Invariant holds during loop execution and after the execution.
+	loop invariant ((b == b + a) && (a == b));
 	loop assigns i, a, b; 
-	loop variant maxlen-1 - i; 				// Variant proves the termination of the loop          
+	loop variant maxlen - 1 - i; 			// Variant proves the termination of the loop          
 	*/
 	for (int i = 2; i < maxlen-1; i++) {
 		int sum = a + b;
@@ -105,10 +130,10 @@ int main() {
 		b = sum;
 	}
 
-	/*@ // Exercise II
+	/*@
 	loop invariant 0 <= j <= maxlen-1;
-	loop assigns j, out[j]; 
-	loop variant  maxlen-1 - j;
+	loop assigns j, out[0 .. maxlen-1]; 
+	loop variant  maxlen -1 - j;
 	*/
 	for (int j = 0; j < maxlen-1; j++) {
 		if (pop(&out[j])) {
@@ -116,9 +141,5 @@ int main() {
 			return -1;
 		}
 	}
-
-	// Exercise III
-	//@ assert ghost_head - 1 == ghost_tail;
-	//@ assert head - 1 == tail;
 	return 0;
 }
